39152d7a80183f3961630289defca50c28004e85
Andrew's PC <Andrew's PC@DESKTOP-04A65L1>
2023-01-30 15:03:08 -0500

    Add project files.

---
+++ Deliverable 3/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace ConsoleProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = 0;
+            string seriesType = "";
+
+            Console.WriteLine("Enter an integer value between 1 and 100:");
+
+            try
+            {
+                input = int.Parse(Console.ReadLine());
+                if (input < 1 || input > 100)
+                {
+                    throw new Exception();
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Invalid input. Please enter an integer value between 1 and 100.");
+                return;
+            }
+
+            Console.WriteLine("Choose a series of integer numbers: (Odd/Even)");
+
+            try
+            {
+                seriesType = Console.ReadLine();
+                if (seriesType.ToLower() != "odd" && seriesType.ToLower() != "even")
+                {
+                    throw new Exception();
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Invalid input. Please choose either 'Odd' or 'Even'.");
+                return;
+            }
+
+            Console.WriteLine($"You have selected the {seriesType} series. The numbers between 0 and {input} are:");
+
+            if (seriesType.ToLower() == "odd")
+            {
+                for (int i = 1; i <= input; i += 2)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            else
+            {
+                for (int i = 0; i <= input; i += 2)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
